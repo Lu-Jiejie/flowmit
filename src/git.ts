7ec1ad8487f1e7f71c57b98918bc2e619929cec0
Lu-Jiejie <lu-jiejie@outlook.com>
2024-05-05 11:29:58 +0800

    fix: changes path

--- src/git.ts
+++ src/git.ts
@@ -23,7 +23,9 @@ export function getUntrackedFiles(): FileInfo[] {
   if (isUndefined(result))
     return []
   return result.split('\n').filter(row => row.trim() !== '')
-    .map(file => ({ name: basename(file), path: basename(file), status: 'U' }))
+    .map((file) => {
+      return { status: 'U', name: basename(file), path: dirname(file) }
+    })
 }
 
 export function getUnstagedTrackedFiles(): FileInfo[] {
